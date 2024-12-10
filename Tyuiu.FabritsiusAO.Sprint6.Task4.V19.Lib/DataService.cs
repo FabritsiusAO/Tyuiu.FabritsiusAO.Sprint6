0ce8b80c403c44cb92165b555a1aaf5284b8528f
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-12-10 22:08:11 +0500

    Task4

--- Tyuiu.FabritsiusAO.Sprint6.Task4.V19.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint6.Task4.V19.Lib/DataService.cs
@@ -1,18 +1,18 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Buffers;
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.FabritsiusAO.Sprint6.Task4.V19.Lib
 {
     public class DataService : ISprint6Task4V19
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] array;
-            int len = startValue - stopValue + 1;
-            array = new double[len];
+            int len = stopValue - startValue + 1;
+            double[] array = new double[len];
+            double y = 0;
             int c = 0;
-            double y;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round(((5 * x + 2.5) / (Math.Sin(x) - 2)) + 2 ,2);
+                y = Math.Round(((5 * x + 2.5) / (Math.Sin(x) - 2)) + 2, 2);
                 if (double.IsInfinity(y))
                 {
                     y = 0;
