e4544d82caa9a5e6afc6ef5b9327d12d16ee72f1
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-12-11 00:41:13 +0500

    Task7

--- Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib/DataService.cs
@@ -5,20 +5,8 @@ namespace Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib
     {
         public int[,] GetMatrix(int[,] matrix)
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                if (matrix[i, 8] != 10)
-                {
-                    matrix[i, 8] = 0;
-                }
-            }
-            return matrix;
-        }
-
-        int[,] ISprint6Task7V12.GetMatrix(string path)
-        {
             int[,] array = new int[10, 10]
-{
+            {
             {17, 17, -7, -19, -4, 6, 4, -17, 0, 13},
             {-13, -13, -7, -15, -12, -1, 15, 18, 0, -19},
             {9, -12, -9, 8, -12, 20, 10, -6, 0, -1},
@@ -29,8 +17,13 @@ namespace Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib
             {-14, -10, -15, 19, 12, -18, 6, 15, 0, -16},
             {19, 18, 14, 5, 8, 4, -13, 10, 0, 20},
             {-7, 3, 12, 7, -1, -13, -6, 3, 0, -8}
-};
+            };
             return array;
+        }
+
+        int[,] ISprint6Task7V12.GetMatrix(string path)
+        {
+            throw new NotImplementedException();
         }
     }
 }
