b0cd729da69741a30171378eb3ecfa1b537a06b9
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-12-11 00:39:09 +0500

    Task7

--- Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib/DataService.cs
@@ -3,25 +3,21 @@ namespace Tyuiu.FabritsiusAO.Sprint6.Task7.V12.Lib
 {
     public class DataService : ISprint6Task7V12
     {
-        public int[,] GetMatrix(string path)
-        {
-            throw new NotImplementedException();
-        }
         public int[,] GetMatrix(int[,] matrix)
         {
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 9; j < cols; j++)
+                if (matrix[i, 8] != 10)
                 {
-                    if (matrix[i, j] != 10)
-                    {
-                        matrix[i, j] = 0;
-                    }
+                    matrix[i, 8] = 0;
                 }
             }
             return matrix;
+        }
+
+        int[,] ISprint6Task7V12.GetMatrix(string path)
+        {
+            throw new NotImplementedException();
         }
     }
 }
